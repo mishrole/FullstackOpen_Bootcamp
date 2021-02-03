f58d1c11a75f138a6db730e8a4e80714a3a1d198
Mitchell Rodríguez <mitrodle@gmail.com>
2021-02-03 11:28:15 -0500

    Add: part1 | courseinfo

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,41 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
-  return (
-    <div>
-      {/* If the value of the prop is achieved using Javascript,
-       it must be wrapped with curly braces */}
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
-
-// React Component names must be capitalized
-const Footer = () => {
-  return (
-    <div>
-      Greeting app created by <a href="https://github.com/mishrole">mishrole</a>
-    </div>
-  )
-}
-
 const App = () => {
-  const now = new Date();
-  const name = 'Mitch';
-  const age = 23;
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of Reat';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component'
+  const exercises3 = 14;
 
-  return(
-  <div>
-    <p>Hello world, it is {now.toString()}</p>
-    <Hello name="Klaus" age={26 + 10} />
-    <Hello name={name} age={age} />
-    {/* If you use <footer /> the page is not going to display the
-    content defined within the Footer component, and instead React
-    only creates an empty footer element */}
-    <Footer />
-
-  </div>
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>{part1} {exercises1}</p>
+      <p>{part2} {exercises2}</p>
+      <p>{part3} {exercises3}</p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
   )
 }
 
